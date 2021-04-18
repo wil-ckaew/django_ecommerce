5bb10d7cced618a8e47969c279b74539dbf2d4ea
wil-ckaew <wil.kimel@gmail.com>
2021-04-18 16:50:50 -0300

    e_commerce/views.py and template/contact/view.html and e_commerce/forms.py was modified

--- e_commerce/views.py
+++ e_commerce/views.py
@@ -1,11 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+
+from .forms import ContactForm
+
+
 def home_page(request):
     context = {
         "title": "Página principal",
         "content": "Bem-vindo a página principal"
     }
     return render(request, "home_page.html", context)
+
 
 def about_page(request):
     context = {
@@ -14,9 +19,20 @@ def about_page(request):
     }
     return render(request, "about/view.html", context)
 
+
 def contact_page(request):
+    contact_form = ContactForm(request.POST or None)
     context = {
+
         "title": "Página de contato",
-        "content": "Bem-vindo a página de contato"
+        "content": "Bem-vindo a página de contato",
+        "form": contact_form
     }
+    if contact_form.is_valid():
+        print(contact_form.cleaned_data)
+        # if request.method == "POST":
+        # print(request.POST)
+        # print(request.POST.get('Nome_Completo'))
+        # print(request.POST.get('email'))
+        # print(request.POST.get('Mensagem'))
     return render(request, "contact/view.html", context)
